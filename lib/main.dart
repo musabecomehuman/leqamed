3c6bd421ffabe3a20776814e0a85ec86e24a7049
musabecomehuman <musakerimov228@gmail.com>
2024-02-16 01:33:32 +0400

    Registration skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,263 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: MyRegistrationScreen(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyRegistrationScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyRegistrationScreenState createState() => _MyRegistrationScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyRegistrationScreenState extends State<MyRegistrationScreen> {
+  bool _emailError = false;
+  String _emailErrorText = '';
+  String _email = '';
+  bool _passwordVisible = false;
+  bool _emailValid = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final emailController = TextEditingController();
+  final focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    focusNode.addListener(() {
+      if (!focusNode.hasFocus) {
+        _checkEmail(_email);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 40),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text(
+              'Регистрация',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color:Color(0xff1C1B19), fontFamily: 'Inter'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          SizedBox(height: 12),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text(
+              'Register to book an appointment, view medical card, and manage family profile',
+              style: TextStyle(fontSize: 14),
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 24),
+          Container(
+            padding: EdgeInsets.only(bottom: 8),
+            child: Text(
+              'Email',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Stack(
+              children: [
+                TextField(
+                  controller: emailController,
+                  focusNode: focusNode,
+                  onChanged: (value) {
+                    setState(() {
+                      _email = value;
+                      _emailError = false;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    labelText: 'Enter your email',
+                    errorText: _emailError ? _emailErrorText : null,
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        emailController.clear();
+                        setState(() {
+                          _email = '';
+                          _emailValid = false;
+                        });
+                      },
+                      child: Icon(
+                        Icons.clear,
+                        color: _email.isNotEmpty ? Colors.black : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 40,
+                  top: 10,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _checkEmail(_email);
+                      });
+                    },
+                    child: Icon(
+                      _emailValid ? Icons.check_circle : Icons.circle_outlined,
+                      color: _emailValid ? Colors.green : Colors.grey,
+                      size: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 24),
+          Container(
+            padding: EdgeInsets.only(bottom: 8),
+            child: Text(
+              _emailError ? 'Invalid email address' : '',
+              style: TextStyle(color: Colors.red, fontSize: 12),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: 8),
+            child: Text(
+              'Password',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: TextField(
+              obscureText: !_passwordVisible,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                labelText: 'Enter your password',
+                suffixIcon: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _passwordVisible = !_passwordVisible;
+                    });
+                  },
+                  child: Icon(
+                    _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 24),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: ElevatedButton(
+              onPressed: () {
+                // Handle registration button press
+              },
+              child: Text('Next'),
+            ),
+          ),
+          SizedBox(height: 24),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 8),
+                GestureDetector(
+                  onTap: () {
+                    // Handle Google registration
+                  },
+                  child: SvgPicture.asset('assets/google_icon.svg', width: 20),
+                ),
+                Text('  Регистрация с Google'),
+              ],
+            ),
+          ),
+          SizedBox(height: 24),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 8),
+                GestureDetector(
+                  onTap: () {
+                    // Handle Apple registration
+                  },
+                  child: SvgPicture.asset('assets/apple_icon.svg', width: 20),
+                ),
+                Text('  Регистрация с Apple'),
+              ],
+            ),
+          ),
+          SizedBox(height: 16),
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                // Handle 'Already have an account? Login' press
+              },
+              child: Text(
+                'Login',
+                style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                    fontSize: 16),
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  void _checkEmail(String email) {
+    // Perform email validation
+    if (email.isNotEmpty) {
+      // Add your custom email validation logic here
+      // For example, check if the email contains '@' and '.com'
+      if (email.contains('@') && email.contains('.com')) {
+        setState(() {
+          _emailValid = true;
+          _emailError = false;
+        });
+      } else {
+        setState(() {
+          _emailValid = false;
+          _emailError = true;
+          _emailErrorText = 'Please enter a valid email address';
+        });
+      }
+    } else {
+      setState(() {
+        _emailValid = false;
+      });
+    }
   }
 }
